790558aab832d9f535114311504d5c0efd91d880
PREDATOR <aniruuddhdhote@gmail.com>
2026-06-18 19:36:47 +0530

    Changed names of the file and added new program

---
+++ Print_in_same_Line.py
@@ -0,0 +1,11 @@
+#When you use 2 print statements, Python By default uses next line to print the next statement
+#But if you want to print line in the same line even if you are using multiple print statement 
+#Use [end=" "] 
+
+print('Without end=" " ')      #Note-We used single quotes here to print double quotes.
+print ("I am a Human")
+print ("My name is Aniruddh")
+
+print('With end=" " ') 
+print ("I am a Human",end=" ")   #Note- We add a space after end=" " for better readability.
+print ("My name is Aniruddh")
